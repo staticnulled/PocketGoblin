fee67bf7fac6c5f74ab37733265a7560a60b1383
Sky <skylerlusc@gmail.com>
2020-01-11 14:10:33 -0500

    Draggable Tile
    Created script to allow for mouse draggable tiles

---
+++ Assets/Box.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+
+    private float startPosX;
+    private float startPosY;
+    private Vector2 mousePos;
+
+    [SerializeField]
+    private bool isBeingHeld = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBeingHeld)
+        {   
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            gameObject.transform.localPosition = new Vector2(mousePos.x - startPosX, mousePos.y - startPosY);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            isBeingHeld = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingHeld = false;
+    }
+}
+
---
+++ Assets/SnapToGrid.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField] private float size = 1f;
+
+    public Vector2 GetNearestPointOnGrid (Vector2 position)
+    {
+        position -= (Vector2)transform.position;
+
+        int xCount = Mathf.RoundToInt(position.x / size);
+        int yCount = Mathf.RoundToInt(position.y / size);
+
+        Vector2 result = new Vector2(
+            (float)xCount * size,
+            (float)yCount * size);
+
+        result += (Vector2)transform.position;
+
+        return result;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+
+        for (float x = 0; x < 40; x+= size)
+        {
+            for (float y = 0; y < 40; y += size)
+            {
+                var point = GetNearestPointOnGrid(new Vector2(x, y));
+                Gizmos.DrawSphere(point, 0.1f);
+            }
+        }
+    }
+}
